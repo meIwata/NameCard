5d5fa207574bd2cfd0be907bff043c847aa72638
grok9312 <218726683+grok9312@users.noreply.github.com>
2025-09-09 15:37:55 +0800

    打出名片內容

--- NameCard/MC/MCView.swift
+++ NameCard/MC/MCView.swift
@@ -5,3 +5,15 @@
 //  Created by Apple on 2025/9/9.
 //
 
+import SwiftUI
+
+struct MCView: View {
+    var body: some View {
+        Image(systemName: "cloud")
+        Text("MC")
+    }
+}
+
+#Preview {
+    MCView()
+}
